b6ad2825e813847d18e988eb4bfd6e1cf778e29d
AbidMaf <abidmafahim@upi.edu>
2022-09-30 07:47:24 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-    <HomePage />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/Post" element={<PostsPage />} />
+        <Route path="/Post" element={<PostsPage />} /> 
       </Routes>
     </div>
   );
